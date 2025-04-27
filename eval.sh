4cbc3a635eb628d5923fac9b8f20140f724ca942
pixeli99 <pixeli@qq.com>
2025-04-27 17:41:10 +0800

    1

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 # 使用 0,1,2 三张卡
-export HF_HOME=/lpai/volumes/ad-vla-vol-ga/lipengxiang/code/LLaDA/huggingface
+export HF_HOME=./
 export HF_ENDPOINT=https://hf-mirror.com
-CUDA_VISIBLE_DEVICES=0,1,2 python3 eval_gsm8k_4shot_parallel.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 eval_gsm8k_4shot_parallel.py \
     --model_name GSAI-ML/LLaDA-8B-Instruct \
     --split test \
     --batch_size 1 \
